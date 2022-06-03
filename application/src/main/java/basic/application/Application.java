7efa724c151c5fc9a97cb52ff331870425342940
ordilov <ordilov@gmail.com>
2022-06-03 18:31:15 +0900

    add multi module

---
+++ application/src/main/java/basic/application/Application.java
@@ -0,0 +1,28 @@
+package basic.application;
+
+import basic.library.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication(scanBasePackages = "basic")
+public class Application {
+
+  private final MyService myService;
+
+  public Application(MyService myService) {
+    this.myService = myService;
+  }
+
+  @GetMapping("/")
+  public String home() {
+    return myService.message();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
